dcbf95e9ac5986946937b77c8bcb189ef5a2053f
melaku05 <melakushiferaw05@gmail.com>
2022-07-14 17:50:45 +0300

    added data process method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+
+  after_save :update_posts_counter
+
+  def self.update_posts_counter(author)
+    author.increment!(:posts_counter)
+  end
+
+  def self.recent_comments(comments)
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def self.recent(max = 3)
+    limit(max).order(created_at: :desc)
+  end
 end
