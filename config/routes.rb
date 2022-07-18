72c1c5f5bbd1e580d3936fc3fc7324de57f5ba4a
melaku05 <melakushiferaw05@gmail.com>
2022-07-18 17:55:34 +0300

    added routes and controller for user and post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/users/user_id/posts",  to: "posts#index", as: :posts
+  get "/users/user_id/posts/:id", to: "posts#show", as: :post
+ 
+  get "/users", to: "users#index", as: :users
+  get "users/h/hello/:id", to: "users#show" ,as: :user
 end
