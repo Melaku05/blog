d43010da953e0445773a56ccf043cf00557c7d22
melaku05 <melakushiferaw05@gmail.com>
2022-07-30 08:45:57 +0300

    add authorization to the blog app

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:new, :create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:new, :create, :destroy]
     end
   end
 end
